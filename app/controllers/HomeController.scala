ae5e55577493c6694efaaf2e045b16dd093c1a5d
deama <deama@mail.com>
2020-03-13 15:20:41 +0000

    first commit

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,10 +4,16 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc)
+{
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index() :Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok("Welcome!").withSession("connected" -> "user@gmail.com")
   }
+
+  def help() :Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(request.session.get("connected").getOrElse("User is not logged in"))
+  }
+
 
 }
