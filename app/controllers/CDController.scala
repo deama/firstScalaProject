b459e75893d917fa5c23fa3bb2ff396c9144ecad
deama <deama@mail.com>
2020-03-17 09:54:42 +0000

    first commit

---
+++ app/controllers/CDController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import javax.inject._
+import models.CD
+import play.api.mvc._
+
+@Singleton
+class CDController @Inject()(cc :ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport
+{
+  def showCDForm() = Action { implicit request :Request[AnyContent] =>
+    Ok( views.html.submitCD(CD.createCDForm) )
+  }
+
+  def submitCD() = Action { implicit request :Request[AnyContent] =>
+    CD.createCDForm.bindFromRequest.fold({ formWithErrors =>
+      BadRequest( views.html.submitCD(formWithErrors) )
+    }, { cd =>
+      Ok(cd.toString())
+    })
+  }
+}
---
+++ app/models/CD.scala
@@ -0,0 +1,22 @@
+package models
+
+import play.api.data.Form
+import play.api.data.Forms._
+
+case class CD(name :String, price :Int)
+{
+  override def toString() =
+  {
+    s"The name of the CD is: $name and the price is: $price"
+  }
+}
+
+object CD
+{
+  val createCDForm :Form[CD] = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "price" -> number
+    )(CD.apply)(CD.unapply)
+  )
+}
